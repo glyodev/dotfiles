eab237d96c2683c3230576d788efd2bfcd511703
glyodev <admin@glyo.dev>
2026-07-16 22:02:03 -0400

    scripts + services

--- .config/shell/paths.sh
+++ .config/shell/paths.sh
@@ -1,9 +1,7 @@
 # PATHS
-PATH=$PATH:/opt/lampp
 PATH=$PATH:~/.config/composer/vendor/bin
-PATH=$PATH:~/.scripts
+PATH=$PATH:~/.scripts/apps
 PATH=$PATH:~/.local/share/gem/ruby/3.4.0/bin
-#PATH=$PATH:/opt/flutter/bin
 PATH=$PATH:~/Android/Sdk/cmdline-tools/latest/bin
 PATH=$PATH:~/Android/Sdk/platform-tools
 PATH=$PATH:~/fvm/default/bin
