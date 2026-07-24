e8924d42c7b23bf4e91597248f08e6720a200935
glyodev <admin@glyo.dev>
2026-07-23 22:25:54 -0400

    export

--- .config/shell/exports.sh
+++ .config/shell/exports.sh
@@ -6,5 +6,7 @@ export EDITOR=vim
 export JAVA_HOME="/usr/lib/jvm/default"
 export PAGER="less -FX"
 export LESS="-FXR"
+export FLUTTER_SKIP_UPDATE_CHECK=true
+export CI=true
 
 # export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=True'
